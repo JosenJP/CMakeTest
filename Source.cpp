6c1b72cd4ce5c8837ebca6c80a274d7d9871ce86
Josen <jiang891203@126.com>
2018-04-23 22:54:55 +0800

    Add some new features in c++11.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!!" << endl;
-	return 0;
+    cout << "Hello world!!" << endl;
+
+    list<int> l_IntList = {1, 2, 3, 4, 5};
+    for (int l_Value : l_IntList)
+    {
+        cout << "Value: " << l_Value << endl;
+    }
+
+    return 0;
 }
